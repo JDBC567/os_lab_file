21fc67aed9777400283a5ba69b1a967ac1708f52
Abhay Singh Chauhan <76419229+JDBC567@users.noreply.github.com>
2024-04-15 22:41:42 +0530

    Update srtf_scheduling.cpp

--- srtf_scheduling.cpp
+++ srtf_scheduling.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
 
 struct Process {
-	int pid; // Process ID
-	int bt; // Burst Time
-	int art; // Arrival Time
+	int pid;
+	int bt;
+	int art; 
 };
 void findWaitingTime(Process proc[], int n,int wt[])
 {
 	int rt[n];
 
-	// Copy the burst time into rt[]
 	for (int i = 0; i < n; i++)
 		rt[i] = proc[i].bt;
 	int complete = 0, t = 0, minm = INT_MAX;
 	int shortest = 0, finish_time;
 	bool check = false;
-	// Process until all processes getscompleted
+
 	while (complete != n) {
 		for (int j = 0; j < n; j++) {
 			if ((proc[j].art <= t) &&
@@ -29,36 +28,27 @@ void findWaitingTime(Process proc[], int n,int wt[])
 			t++;
 			continue;
 		}
-		// Reduce remaining time by one
 		rt[shortest]-=1;
-		// Update minimum
 		minm = rt[shortest];
 		if (minm == 0)
 			minm = INT_MAX;
-		// If a process gets completely executed
 		if (rt[shortest] == 0) {
-			// Increment complete
 			complete++;
 			check = false;
-			// Find finish time of current process
 			finish_time = t + 1;
-			// Calculate waiting time
 			wt[shortest] = finish_time -proc[shortest].bt - proc[shortest].art;
 			if (wt[shortest] < 0)
 				wt[shortest] = 0;
 		}
-		// Increment time
 		t++;
 	}
 }
 
 void findTurnAroundTime(Process proc[], int n,int wt[], int tat[])
 {
-	// calculating turnaround time by adding bt[i] + wt[i]
 	for (int i = 0; i < n; i++)
 		tat[i] = proc[i].bt + wt[i];
 }
-// Function to calculate average time
 void findavgTime(Process proc[], int n)
 {
 	int wt[n], tat[n], total_wt = 0,total_tat = 0;
